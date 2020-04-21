c66b8ff011ec626f65760f22209ae4e638480d90
yqq14978 <18382258214@163.com>
2020-04-21 21:21:37 +0800

    first commit

---
+++ src/collectionDemo/DemoTest.java
@@ -0,0 +1,30 @@
+package collectionDemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DemoTest {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList(6);
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+//        System.out.println(list.size());
+//        HashMap hashMap = new HashMap();
+//        hashMap.put();
+//        Collections.synchronizedMap();
+//        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+//        concurrentHashMap.put("" , "");
+//        Hashtable hashtable = new Hashtable();
+//        hashtable.put();
+    }
+
+}
