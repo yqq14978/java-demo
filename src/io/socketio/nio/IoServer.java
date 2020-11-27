1e758ecb1a4f0ac4a99c7f851a7d34f0bf854600
yeqiangqiang <18382258214@163.com>
2020-11-27 10:59:32 +0800

    更改类的位置

--- src/io/socketio/nio/IoServer.java
+++ src/io/socketio/nio/IoServer.java
@@ -1,4 +1,4 @@
-package io.socketio;
+package io.socketio.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
--- src/io/socketio/nio/ZeroCopyClient.java
+++ src/io/socketio/nio/ZeroCopyClient.java
@@ -1,4 +1,4 @@
-package io.socketio;
+package io.socketio.nio;
 
 import java.io.FileInputStream;
 import java.io.IOException;
