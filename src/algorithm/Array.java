4a94548a7775c1ac414f91e0428bf2e54b1259bf
yeqiangqiang <18382258214@163.com>
2021-01-19 16:21:50 +0800

    算法刷题

--- src/algorithm/Array.java
+++ src/algorithm/Array.java
@@ -10,7 +10,9 @@ package algorithm;
 public class Array {
 
     public static void main(String[] args) {
-        removeDuplicates();
+//        removeDuplicates();
+//        pivotIndex();
+        searchInsert();
     }
 
     /**
@@ -32,5 +34,91 @@ public class Array {
             }
         }
         System.out.println(i + 1);
+    }
+
+    /**
+    * @Author yeqq
+    * @Description 寻找数组的中心索引，即该索引左边所有元素的和等于右边所有元素的和，没有中心索引则返回-1
+    * @Date 10:50 2021/1/19
+    * @Param []
+    * @return void
+    **/
+    public static void pivotIndex(){
+        int[] nums = {1,7,3,6,5,6};
+        int sum = 0;
+        int pivotIndex = -1;
+        int leftCount = 0;
+        for (int x: nums) {
+            sum += x;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftCount == sum - leftCount - nums[i]){
+                pivotIndex = i;
+                break;
+            }
+            leftCount += nums[i];
+        }
+        System.out.println("右边元素和：" + leftCount + "——左边元素和：" + leftCount);
+        System.out.println("中心索引：" + pivotIndex);
+    }
+    
+    /**
+    * @Author yeqq
+    * @Description 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中
+    * ，返回它将会被按顺序插入的位置。(最优解，二分查找)
+    * @Date 14:23 2021/1/19
+    * @Param []
+    * @return void
+    **/
+    public static void searchInsert(){
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while (left <= right){
+            mid = (left + right) / 2;
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else if(nums[mid] > target){
+                right = mid - 1;
+            }else if (nums[mid] == target){
+                System.out.println(mid);
+                return;
+            }
+        }
+        System.out.println(left);
+//        int midIndex = nums.length / 2;
+//        int targetIndex = -1;
+//        if(target > nums[midIndex]){
+//            for(int i = midIndex + 1; i < nums.length; i++){
+//                if(target < nums[i]){
+//                    targetIndex = i;
+//                    break;
+//                }else if(target == nums[i]){
+//                    targetIndex = i;
+//                    break;
+//                }
+//            }
+//            if(targetIndex == -1){
+//                targetIndex = nums.length;
+//            }
+//        }else if(target < nums[midIndex]){
+//            for(int i = midIndex - 1; i >= 0; i--){
+//                if(target > nums[i]){
+//                    targetIndex = i + 1;
+//                    break;
+//                }else if(target == nums[i]){
+//                    targetIndex = i;
+//                    break;
+//                }
+//            }
+//            if(targetIndex == -1){
+//                targetIndex = 0;
+//            }
+//        }else {
+//            targetIndex = midIndex;
+//        }
+//        System.out.println(targetIndex);
     }
 }
